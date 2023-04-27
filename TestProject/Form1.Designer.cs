7695e3ce18e83fa6ef2eb6e59cb34f199f40908e
azam1 <azam1@ZIDDISMACHINE>
2023-04-27 07:54:05 +0600

    Add project files.

---
+++ TestProject/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace TestProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.TTS = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(135, 98);
+            this.textBox.Multiline = true;
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(331, 236);
+            this.textBox.TabIndex = 0;
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            this.textBox.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.textBox_PreviewKeyDown);
+            // 
+            // TTS
+            // 
+            this.TTS.Location = new System.Drawing.Point(556, 202);
+            this.TTS.Name = "TTS";
+            this.TTS.Size = new System.Drawing.Size(157, 43);
+            this.TTS.TabIndex = 1;
+            this.TTS.Text = "TTS";
+            this.TTS.UseVisualStyleBackColor = true;
+            this.TTS.Click += new System.EventHandler(this.TTS_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TTS);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Simple TTS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button TTS;
+    }
+}
+
---
+++ TestProject/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TestProject
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool capsLockState = Control.IsKeyLocked(Keys.CapsLock);
+        private void TTS_Click(object sender, EventArgs e)
+        {
+            synth.SpeakAsync(textBox.Text);
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox.TextLength > 0)
+            {
+                synth.SpeakAsync(textBox.Text[textBox.TextLength - 1].ToString());
+            }
+            //synth.SpeakAsync(textBox.Text);
+        }
+
+        private void textBox_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.CapsLock)
+            {
+                // Toggle the Caps Lock state and speak the appropriate message
+                capsLockState = !capsLockState;
+                if (capsLockState)
+                {
+                    synth.SpeakAsync("Caps Lock on");
+                }
+                else
+                {
+                    synth.SpeakAsync("Caps Lock off");
+                }
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+                // Speak "Space" when the Space key is pressed
+                synth.SpeakAsync("Space");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.OemPeriod)
+            {
+                // Speak "period" when the period key is pressed
+                synth.SpeakAsync("OemPeriod");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.Oemcomma)
+            {
+                // Speak "Comma" when the comma key is pressed
+                synth.SpeakAsync("Comma");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.OemSemicolon)
+            {
+                // Speak "Semicolon" when the semicolon key is pressed
+                synth.SpeakAsync("Semicolon");
+                e.IsInputKey = true;
+            }
+            else if (e.KeyCode == Keys.OemQuotes)
+            {
+                // Speak "Quotes" when the quotes key is pressed
+                synth.SpeakAsync("Quotes");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.OemBackslash)
+            {
+                // Speak "Backslash" when the backslash key is pressed
+                synth.SpeakAsync("Backslash");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.OemOpenBrackets)
+            {
+                // Speak "Open bracket" when the open bracket key is pressed
+                synth.SpeakAsync("Open bracket");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.OemCloseBrackets)
+            {
+                // Speak "Close bracket" when the close bracket key is pressed
+                synth.SpeakAsync("Close bracket");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.OemMinus)
+            {
+                // Speak "Minus" when the minus key is pressed
+                synth.SpeakAsync("Minus");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.Oem1)
+            {
+                // Speak "Semicolon" when the semicolon key is pressed
+                synth.SpeakAsync("Semicolon");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.Oem2)
+            {
+                // Speak "Forward slash" when the forward slash key is pressed
+                synth.SpeakAsync("Forward slash");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.Oem3)
+            {
+                // Speak "Tilde" when the tilde key is pressed
+                synth.SpeakAsync("Tilde");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.Oem4)
+            {
+                // Speak "Open bracket" when the open bracket key is pressed
+                synth.SpeakAsync("Open bracket");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.Oem5)
+            {
+                // Speak "Backslash" when the backslash key is pressed
+                synth.SpeakAsync("Backslash");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.Oem6)
+            {
+                // Speak "Close bracket" when the close bracket key is pressed
+                synth.SpeakAsync("Close bracket");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.Oem7)
+            {
+                // Speak "Quotes" when the quotes key is pressed
+                synth.SpeakAsync("Quotes");
+                e.IsInputKey = true; 
+            }
+            else if (e.KeyCode == Keys.Oem8)
+            {
+                // Speak "Backslash" when the backslash key is pressed
+                synth.SpeakAsync("Backslash");
+                e.IsInputKey = true; 
+            }
+        }
+    }
+}
